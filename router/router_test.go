b7819e672fb058e184637b2345dcb794bf79d861
jetiny <86267344@qq.com>
2018-04-28 09:52:25 +0800

    howto?

--- router/router_test.go
+++ router/router_test.go
@@ -2,8 +2,51 @@ package router
 
 import (
   "testing"
+  "encoding/json"
+  "fmt"
 )
 
+type Route struct {
+  Name string
+  Path string
+  Modal string
+}
+
+type Modal struct {
+  Name string
+  Routes []Route
+}
+
+type Contract struct {
+  Modals []Modal
+  Routes []Route
+}
+
 func TestRouter(t * testing.T) {
-  
+  jsonText := []byte( `{
+    "modals": [
+      {
+        "name": "Account",
+        "routes": [
+          {
+            "name": "login"
+          }
+        ]
+      },
+      {
+        "name": "Home"
+      }
+    ],
+    "routes": [
+      {
+        "modal": "Home",
+        "name": "About"
+      }
+    ]
+  }` )
+  var jsonObject Contract
+  json.Unmarshal(jsonText, &jsonObject)
+
+  text, _ := json.Marshal(jsonObject)
+  fmt.Println(string(text))
 }
